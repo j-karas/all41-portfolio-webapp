b8a413f9c0786f7e5ca647a2590c7e7af57f24b8
John Karas <tuj47543@temple.edu>
2023-06-09 17:54:16 -0400

    Updated the paragraphs for the home page and about page. Also added link to the logo on the navBar where it sends the user back to the landing page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,74 +5,85 @@ const Home = () => {
 return (
   <>
     <NavBar />
-    <div className="container mx-auto px-6">
-      <h1 className="text-indianRed">ALL41</h1>
-    </div>
-    <button
-      id="dropdownDefaultButton"
-      data-dropdown-toggle="dropdown"
-      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      type="button"
-    >
-      Dropdown button{" "}
-      <svg
-        className="w-4 h-4 ml-2"
-        aria-hidden="true"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M19 9l-7 7-7-7"
-        ></path>
-      </svg>
-    </button>
-    <div
-      id="dropdown"
-      className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-    >
-      <ul
-        className="py-2 text-sm text-gray-700 dark:text-gray-200"
-        aria-labelledby="dropdownDefaultButton"
-      >
-        <li>
-          <a
-            href="#"
-            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-          >
-            Dashboard
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-          >
-            Settings
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-          >
-            Earnings
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-          >
-            Sign out
-          </a>
-        </li>
-      </ul>
-    </div>
+    <figure className="max-w-lg items-center mx-auto mt-4 m-8 bg-onyx rounded-md drop-shadow-md">
+      <div className="p-8 pb-2 text-left space-y-4 text-seaSalt">
+        Introducing All41: A Fusion of Custom Clothing, Hand-Drawn Art,
+        Photography, Tufting, Coding, and Artisan Craftsmanship 
+      </div>
+      <div className="p-8 pt-2 pb-2 text-left space-y-4 text-seaSalt">
+        Welcome to All41, a dynamic brand that seamlessly weaves together the realms of
+        custom clothing, hand-drawn art, captivating photography, mesmerizing
+        tufting, innovative coding, and exquisite handcrafted projects. All41 is
+        a vibrant tapestry of creativity, where diverse forms of artistic
+        expression converge to create an unparalleled experience.
+      </div>
+      <div className="p-8 pt-2 pb-2 text-left space-y-4 text-seaSalt">
+        At the heart of All41 lies a deep commitment to crafting custom clothing that
+        transcends boundaries and celebrates individual style. Step into the
+        world of All41's custom clothing collection, where every garment is a
+        canvas for self-expression. From tailored suits that exude
+        sophistication to unique dresses that capture the essence of elegance,
+        each piece is meticulously crafted to perfection. All41's skilled
+        artisans bring your vision to life, ensuring that your clothing reflects
+        your personality, empowers your confidence, and becomes an extension of
+        your identity.
+      </div>
+      <div className="p-8 pt-2 pb-2 text-left space-y-4 text-seaSalt">
+        In the realm of art, All41's hand-drawn creations take
+        center stage, showcasing the talent of visionary artists. Immerse
+        yourself in a gallery of captivating artworks that stir emotions,
+        challenge perceptions, and inspire imagination. From intricate
+        illustrations that tell enchanting stories to bold strokes of creativity
+        that provoke thought, All41 curates a collection that showcases the
+        diversity and power of hand-drawn art. Every piece featured is a
+        testament to the brand's commitment to nurturing artistic talent and
+        fostering a deep appreciation for the beauty of the human touch.
+      </div>
+      <div className="p-8 pt-2 pb-2 text-left space-y-4 text-seaSalt">
+        All41's devotion to visual storytelling is further manifested in its
+        captivating photography. Explore a world captured through the lens,
+        where moments frozen in time evoke a myriad of emotions. Whether it's
+        breathtaking landscapes that transport you to distant realms or intimate
+        portraits that reveal the essence of humanity, All41's photography
+        captures the essence of life, inviting you to immerse yourself in
+        captivating narratives and explore the world from a fresh perspective.
+      </div>
+      <div className="p-8 pt-2 pb-2 text-left space-y-4 text-seaSalt">
+        Venture into the art of tufting, where threads are transformed into
+        extraordinary tapestries, rugs, and wall hangings. All41's tufting
+        creations blend traditional techniques with contemporary designs,
+        resulting in mesmerizing pieces that transcend the boundaries of art and
+        function. Each tufted masterpiece carries a story woven into its fibers,
+        creating a tactile and visual experience that adds depth and character
+        to any space.
+      </div>
+      <div className="p-8 pt-2 pb-2 text-left space-y-4 text-seaSalt">
+        All41 embraces the transformative power of coding, where
+        innovation meets technology. The brand pushes boundaries in the digital
+        realm, designing immersive experiences, developing cutting-edge
+        applications, and crafting seamless websites. All41's coding endeavors
+        bring together artful design and technical expertise to create digital
+        experiences that leave a lasting impression, fostering connection and
+        enabling users to explore new frontiers.
+      </div>
+      <div className="p-8 pt-2 pb-2 text-left space-y-4 text-seaSalt">
+        The spirit of artisan craftsmanship permeates All41's handcrafted projects, where passion and
+        skill converge. From bespoke leather goods that exude timeless charm to
+        intricately designed ceramics that showcase the beauty of tactile art,
+        All41's handcrafted treasures embody the brand's commitment to
+        preserving traditional techniques while infusing them with contemporary
+        aesthetics. Each handcrafted piece tells a story of dedication and
+        excellence, celebrating the artistry of human hands.
+      </div>
+      <div className="p-8 pt-2 text-left space-y-4 text-seaSalt">
+        All41 is a rich tapestry of creativity, where custom clothing, hand-drawn art,
+        photography, tufting, coding, and artisan craftsmanship intertwine. It's
+        a brand that invites you to explore, connect, and express yourself
+        through a multitude of artistic mediums. Step into the realm of All41
+        and experience a world where boundaries fade, inspiration flourishes,
+        and the magic of artistic expression knows no limits.
+      </div>
+    </figure>
   </>
 );
 };
